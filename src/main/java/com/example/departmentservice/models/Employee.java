24461cd8b24fb5006b3c8541942cb84b92fb22a7
Liliana Everett <everettliliana@gmail.com>
2019-10-22 12:03:43 +0100

    add other classes and Jparepositories

---
+++ src/main/java/com/example/departmentservice/models/Employee.java
@@ -0,0 +1,98 @@
+package com.example.departmentservice.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "employee_number")
+    private int employee_number;
+
+    @JsonIgnoreProperties("employees")
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
+    @JsonIgnoreProperties("projects")
+    @ManyToMany
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = {@JoinColumn (
+                    name = "employee_id",
+                    nullable = false,
+                    updatable = false)
+
+            },
+            inverseJoinColumns = {@JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+                    updatable = false)
+            }
+    )
+    private List<Project> projects;
+
+    public Employee(String first_name, String last_name, int employee_number, Department department) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.employee_number = employee_number;
+        this.department = department;
+        this.projects = new ArrayList<>();
+    }
+
+    public Employee() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getEmployee_number() {
+        return employee_number;
+    }
+
+    public void setEmployee_number(int employee_number) {
+        this.employee_number = employee_number;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
--- src/test/java/com/example/departmentservice/DepartmentserviceApplicationTests.java
+++ src/test/java/com/example/departmentservice/DepartmentserviceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.departmentservice;
 
+import com.example.departmentservice.models.Department;
+import com.example.departmentservice.repositories.DepartmentRepository;
+import com.example.departmentservice.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DepartmentserviceApplicationTests {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Autowired
+	DepartmentRepository departmentRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void createDepartmentAndEmployee() {
+		Department department = new Department("Front End");
+		departmentRepository.save(department);
+	}
 }
